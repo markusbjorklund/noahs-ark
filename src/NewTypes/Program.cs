de2c7249947792da2d12f9bedad9fefc9a716dfb
janne-nylund <janne.nylund@centria.fi>
2020-04-07 19:38:31 +0300

    Create Main method

--- src/NewTypes/Program.cs
+++ src/NewTypes/Program.cs
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-  
->>>>>>> 5f67b85b68d9e9bcde83a76b6b32529b66214ebd
 using System;
 using Pets;
 
@@ -12,13 +8,9 @@ namespace ConsoleApplication
     public static void Main(string[] args)
     {
       Dog doggie = new Dog();
-<<<<<<< HEAD
       Cat cattie = new Cat();
       Console.WriteLine(doggie.TalkToOwner());
       Console.WriteLine(cattie.TalkToOwner());
-=======
-      Console.WriteLine(doggie.TalkToOwner());
->>>>>>> 5f67b85b68d9e9bcde83a76b6b32529b66214ebd
     }
   }
 }
