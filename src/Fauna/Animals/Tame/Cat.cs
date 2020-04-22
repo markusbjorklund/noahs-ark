426c7923c489c07a3de3b6d498151dbfd58754b8
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-22 20:03:19 +0300

    new cat

--- src/Fauna/Animals/Tame/Cat.cs
+++ src/Fauna/Animals/Tame/Cat.cs
@@ -1,12 +1,27 @@
 using System;
 
-namespace Pets
+namespace Animals
 {
-  public class Cat
+  public class Cat : Animal, ITame
   {
+    public string name { get; set; }
+    public Cat(string name)
+    {
+      this.name = name;
+    }
     public string TalkToOwner()
     {
       return "Meow!";
     } 
+     public override string MakeSound()
+    {
+      return "Meow!";
+    }
+    
+    public string ComeHere()
+    {
+      return this.name + " came to the owner.";
+    }
+  
   }
 }
