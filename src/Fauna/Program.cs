44da5a64b1ecc74b56cee1432db1389585f051b5
janne-nylund <janne.nylund@centria.fi>
2020-04-23 07:49:33 +0300

    Add Horse to Program.cs

--- src/Fauna/Program.cs
+++ src/Fauna/Program.cs
@@ -16,9 +16,11 @@ namespace ConsoleApplication
       Bird birdie = new Bird("Roadrunner");
       Cat cattie = new Cat("Garfield");
       Dog doggie = new Dog("Scooby-Doo");
+      Horse heppa = new Horse("Jolly Jumper");
       Console.WriteLine(birdie.TalkToOwner());
       Console.WriteLine(cattie.TalkToOwner()); 
-      Console.WriteLine(doggie.TalkToOwner()); 
+      Console.WriteLine(doggie.TalkToOwner());
+      Console.WriteLine(heppa.TalkToOwner());  
     }
   }
 }
