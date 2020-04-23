87a7c13bcb1da7932a460aa0c13994eed6876ed8
janne-nylund <janne.nylund@centria.fi>
2020-04-23 07:07:48 +0300

    Create Horse.cs

---
+++ src/Fauna/Animals/Tame/Horse.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Animals
+{
+  public class Horse : Animal, ITame
+  {
+    public string name { get; set; }
+    public Horse(string name)
+    {
+      this.name = name;
+    }
+    public string TalkToOwner()
+    {
+      return "Ihahaa!";
+    } 
+     public override string MakeSound()
+    {
+      return "Ihahaa!";
+    }
+    
+    public string ComeHere()
+    {
+      return this.name + " came to the owner.";
+    }
+  
+  }
+}
