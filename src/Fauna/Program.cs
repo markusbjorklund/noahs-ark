aabecf384d4f93fd7cac152b71fbfaf6712b3705
janne-nylund <janne.nylund@centria.fi>
2020-04-23 08:44:02 +0300

    Test Hunt() method

--- src/Fauna/Program.cs
+++ src/Fauna/Program.cs
@@ -17,12 +17,15 @@ namespace ConsoleApplication
       Cat cattie = new Cat("Garfield");
       Dog doggie = new Dog("Scooby-Doo");
       Horse heppa = new Horse("Jolly Jumper");
+      Bear brownie = new Bear();
+
       Console.WriteLine(birdie.TalkToOwner());
       Console.WriteLine(cattie.TalkToOwner()); 
       Console.WriteLine(doggie.TalkToOwner());
       Console.WriteLine(heppa.TalkToOwner());
       Console.WriteLine(heppa.ComeHere());
-      Console.WriteLine(heppa.MakeSound());  
+      Console.WriteLine(heppa.MakeSound());
+      Console.WriteLine(brownie.Hunt());  
     }
   }
 }
