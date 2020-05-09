308dbd3b533626b4b5eab3e55c83fd5f175efa28
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-09 15:47:17 +0300

    interface for wild animals working as intended

--- src/Fauna/Program.cs
+++ src/Fauna/Program.cs
@@ -8,17 +8,13 @@ namespace ConsoleApplication
   {
     public static void Main(string[] args)
     {
+      string animals = "1 - Wolf \n2 - Tiger \n3 - Bear \n4 - Bird \n5 - Cat \n6 - Dog \n7 - Horse";
+      string wildNav = "What do you want this wild beast to do?\nM - Make sound \nH - Hunt";
+      string tameNav = "Cuddle? Or do something of the following?\nE - Eat \nT - Talk to owner \nM - Make sound \nN - Give name";
 
-      string wildAnimals = "1 - Wolf \n2 - Tiger \n3 - Bear \n";
-      string tameAnimals = "4 - Bird \n5 - Cat \n6 - Dog \n7 - Horse";
-      string wildHunts = "M - Make sound \nH - Hunt";
-      string tameEats = "E - Eat \nT - Talk to owner \nM - Make sound \nN - Give name";
+      string topNav = "What do you want to do? \nL - List all animals\nQ - Quit ";
 
-      string question = "What do you want to do?";
-      string initialOptions = "Q - Quit \nL - List all animals";
-
-      Console.WriteLine(question);
-      Console.WriteLine(initialOptions);
+      Console.WriteLine(topNav);
 
       while (true)
       {
@@ -34,15 +30,91 @@ namespace ConsoleApplication
         // list animals
         if (navigation == "L" || navigation == "l")
         {
-          Console.WriteLine(wildAnimals + tameAnimals);
+          Console.WriteLine("\n==================================\n");
+          Console.WriteLine("Choose animal with [1-7]\n\n" + animals);
+          Console.WriteLine("\n==================================\n");
         }
 
         /* wolf */
         if (navigation == "1")
         {
-          string name = "Wolf";
-          Console.WriteLine("You have chosen " + name);
-          Console.WriteLine(question + "\n" + wildHunts);
+          Wolf alpha = new Wolf();
+          Console.WriteLine("\n#########################################################\n");
+          Console.WriteLine("Good choice. Here is your wolf!");
+          Console.WriteLine("\n#########################################################\n");
+          Console.WriteLine(wildNav);
+          {
+            string method = Console.ReadLine();
+            if (method == "M" || method == "m")
+            {
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(alpha.MakeSound());
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(topNav);
+            }
+            if (method == "H" || method == "h")
+            {
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(alpha.Hunt());
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(topNav);
+            }
+          }
+        }
+
+        /* tiger */
+        if (navigation == "2")
+        {
+          Tiger dofus = new Tiger();
+          Console.WriteLine("\n#########################################################\n");
+          Console.WriteLine("I see that you like stripes. Or tigers...");
+          Console.WriteLine("\n#########################################################\n");
+          Console.WriteLine(wildNav);
+          {
+            string method = Console.ReadLine();
+            if (method == "M" || method == "m")
+            {
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(dofus.MakeSound());
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(topNav);
+            }
+            if (method == "H" || method == "h")
+            {
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(dofus.Hunt());
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(topNav);
+            }
+          }
+        }
+
+        /* bear */
+        if (navigation == "3")
+        {
+          Bear brownie = new Bear();
+          Console.WriteLine("\n#########################################################\n");
+          Console.WriteLine("Here is bear. Hide the honey.");
+          Console.WriteLine("\n#########################################################\n");
+          Console.WriteLine(wildNav);
+          {
+            string method = Console.ReadLine();
+            if (method == "M" || method == "m")
+            {
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(brownie.MakeSound());
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(topNav);
+            }
+            if (method == "H" || method == "h")
+            {
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(brownie.Hunt());
+              Console.WriteLine("\n#########################################################\n");
+              Console.WriteLine(topNav);
+
+            }
+          }
         }
       }
     }
