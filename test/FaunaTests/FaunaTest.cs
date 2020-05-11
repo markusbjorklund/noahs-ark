c0047e97d85322661d1c7f1664453bc03a46e00d
janne-nylund <janne.nylund@centria.fi>
2020-05-11 15:30:29 +0300

    Modify Wolf Hunt()

--- test/FaunaTests/FaunaTest.cs
+++ test/FaunaTests/FaunaTest.cs
@@ -4,139 +4,139 @@ using NUnit.Framework;
 
 public class AnimalTests
 {
-  // talk to owner test
-  [Test]
-  public void DogTalkToOwnerReturnsWoof()
-  {
-    string expected = "Woof!";
-    string actual = new Dog("Max").TalkToOwner();
+    // talk to owner test
+    [Test]
+    public void DogTalkToOwnerReturnsWoof()
+    {
+        string expected = "Woof!";
+        string actual = new Dog("Max").TalkToOwner();
 
-    Assert.AreEqual(expected, actual, "Max is the name of the dog!");
-  }
+        Assert.AreEqual(expected, actual, "Max is the name of the dog!");
+    }
 
-  [Test]
-  public void CatTalkToOwnerReturnsMeow()
-  {
-    string expected = "Meow!";
-    string actual = new Cat("Garfield").TalkToOwner();
+    [Test]
+    public void CatTalkToOwnerReturnsMeow()
+    {
+        string expected = "Meow!";
+        string actual = new Cat("Garfield").TalkToOwner();
 
-    Assert.AreEqual(expected, actual, "Garfield is the name of the cat!");
-  }
+        Assert.AreEqual(expected, actual, "Garfield is the name of the cat!");
+    }
 
-  [Test]
-  public void BirdTalkToOwnerReturnsMeow()
-  {
-    string expected = "Tweet!";
-    string actual = new Bird("Roadrunner").TalkToOwner();
+    [Test]
+    public void BirdTalkToOwnerReturnsMeow()
+    {
+        string expected = "Tweet!";
+        string actual = new Bird("Roadrunner").TalkToOwner();
 
-    Assert.AreEqual(expected, actual, "Bird does not say that!");
-  }
+        Assert.AreEqual(expected, actual, "Bird does not say that!");
+    }
 
-  [Test]
-  public void HorseTalkToOwnerReturnsIhahaa()
-  {
-    string expected = "Ihahaa!";
-    string actual = new Horse("Heppa").TalkToOwner();
+    [Test]
+    public void HorseTalkToOwnerReturnsIhahaa()
+    {
+        string expected = "Ihahaa!";
+        string actual = new Horse("Heppa").TalkToOwner();
 
-    Assert.AreEqual(expected, actual, "Horse should say Ihahaa!");
-  }
+        Assert.AreEqual(expected, actual, "Horse should say Ihahaa!");
+    }
 
-  // make sound tests
-  [Test]
-  public void DogMakethisSound()
-  {
-    string expected = "Woof!";
-    string actual = new Dog("Max").MakeSound();
+    // make sound tests
+    [Test]
+    public void DogMakethisSound()
+    {
+        string expected = "Woof!";
+        string actual = new Dog("Max").MakeSound();
 
-    Assert.AreEqual(expected, actual, "Max is the name of the dog!");
-  }
+        Assert.AreEqual(expected, actual, "Max is the name of the dog!");
+    }
 
-  [Test]
-  public void CatMakeThisSound()
-  {
-    string expected = "Meow!";
-    string actual = new Cat("Garfield").MakeSound();
+    [Test]
+    public void CatMakeThisSound()
+    {
+        string expected = "Meow!";
+        string actual = new Cat("Garfield").MakeSound();
 
-    Assert.AreEqual(expected, actual, "Garfield is the name of the cat!");
-  }
+        Assert.AreEqual(expected, actual, "Garfield is the name of the cat!");
+    }
 
-  [Test]
-  public void BirdMakeThisSound()
-  {
-    string expected = "Tweet!";
-    string actual = new Bird("Roadrunner").MakeSound();
+    [Test]
+    public void BirdMakeThisSound()
+    {
+        string expected = "Tweet!";
+        string actual = new Bird("Roadrunner").MakeSound();
 
-    Assert.AreEqual(expected, actual, "Bird does not say that!");
-  }
+        Assert.AreEqual(expected, actual, "Bird does not say that!");
+    }
 
-  [Test]
-  public void HorseMakeThisSound()
-  {
-    string expected = "Hello!";
-    string actual = new Horse("Heppa").MakeSound();
+    [Test]
+    public void HorseMakeThisSound()
+    {
+        string expected = "Hello!";
+        string actual = new Horse("Heppa").MakeSound();
 
-    Assert.AreEqual(expected, actual, "Horse should say Ihahaa!");
-  }
+        Assert.AreEqual(expected, actual, "Horse should say Ihahaa!");
+    }
 
-  [Test]
-  public void BearMakeThisSound()
-  {
-    string expected = "Roar!";
-    string actual = new Bear().MakeSound();
+    [Test]
+    public void BearMakeThisSound()
+    {
+        string expected = "Roar!";
+        string actual = new Bear().MakeSound();
 
-    Assert.AreEqual(expected, actual, "Bear does not say that!");
-  }
+        Assert.AreEqual(expected, actual, "Bear does not say that!");
+    }
 
-  [Test]
-  public void TigerMakeThisSound()
-  {
-    string expected = "Grrr!";
-    string actual = new Tiger().MakeSound();
+    [Test]
+    public void TigerMakeThisSound()
+    {
+        string expected = "Grrr!";
+        string actual = new Tiger().MakeSound();
 
-    Assert.AreEqual(expected, actual, "Tiger does not say that!");
-  }
+        Assert.AreEqual(expected, actual, "Tiger does not say that!");
+    }
 
-  [Test]
-  public void WolfMakeThisSound()
-  {
-    string expected = "Owooooo!";
-    string actual = new Wolf().MakeSound();
+    [Test]
+    public void WolfMakeThisSound()
+    {
+        string expected = "Owooooo!";
+        string actual = new Wolf().MakeSound();
 
-    Assert.AreEqual(expected, actual, "Wolf does not say that!");
-  }
+        Assert.AreEqual(expected, actual, "Wolf does not say that!");
+    }
 
-  // come here tests
-  [Test]
-  public void BirdComeHere()
-  {
-    string actual = new Bird("Roadrunner").ComeHere(); 
-    string expected = actual.ToString();
+    // come here tests
+    [Test]
+    public void BirdComeHere()
+    {
+        string actual = new Bird("Roadrunner").ComeHere();
+        string expected = actual.ToString();
 
-    Assert.AreEqual(expected, actual, "The method does not work!");
-  }
+        Assert.AreEqual(expected, actual, "The method does not work!");
+    }
 
     // test for hunts
-  [Test]
-  public void WolfIsHunting()
-  {
-    string actual = new Wolf().Hunt();
-    string expected = "Wolf hunts.";
+    [Test]
+    public void WolfIsHunting()
+    {
+        string actual = new Wolf().Hunt();
 
-    Assert.AreEqual(expected, actual, "Something is wrong with the hunt method!");
-  }
+        Assert.That(actual, Does.Match("Wolf finds food, gains 1kg and weighs now 1 kg. | Wolf does not find food and weighs now 0 kg"));
 
-  [Test]
-  public void BearIsHunting()
-  {
-    string actual = new Bear().Hunt();
-    
-    Assert.That(actual, Does.Match("Bear finds food, gains 1kg and weighs now 1 kg. | Bear does not find food and weighs now 0 kg"));
-  }
-  
-  [Test]
-  public void TigerIsHunting()
-  {
-    string actual = new Tiger().Hunt();
-    Assert.That(actual, Does.Match("Tiger finds food, gains 1kg and weighs now 1 kg. | Tiger does not find food and weighs now 0 kg"));
-  }
+    }
+
+    [Test]
+    public void BearIsHunting()
+    {
+        string actual = new Bear().Hunt();
+
+        Assert.That(actual, Does.Match("Bear finds food, gains 1kg and weighs now 1 kg. | Bear does not find food and weighs now 0 kg"));
+    }
+
+    [Test]
+    public void TigerIsHunting()
+    {
+        string actual = new Tiger().Hunt();
+        Assert.That(actual, Does.Match("Tiger finds food, gains 1kg and weighs now 1 kg. | Tiger does not find food and weighs now 0 kg"));
+    }
 }
