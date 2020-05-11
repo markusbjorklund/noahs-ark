fd66c216c75bbb570e5f3045c69f61ada59e598e
janne-nylund <janne.nylund@centria.fi>
2020-05-11 13:04:06 +0300

    Update Bear Hunt()

--- src/Fauna/Animals/Wild/Bear.cs
+++ src/Fauna/Animals/Wild/Bear.cs
@@ -2,15 +2,32 @@ using System;
 
 namespace Animals
 {
-  public class Bear : Animal, IWild
-  {
-    public override string MakeSound()
+    public class Bear : Animal, IWild
     {
-      return "Roar!";
+        public int weight { get; set; }
+
+        public Bear()
+        {
+            this.weight = 0;
+        }
+
+        public override string MakeSound()
+        {
+            return "Roar!";
+        }
+        public string Hunt()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 11);
+            if (num <= 6)
+            {
+                this.weight += 1;
+                return this.GetType().Name + " finds food, gains 1 kg and weighs now " + this.weight + " kg.";
+            }
+            else
+            {
+              return this.GetType().Name + " does not find food and weighs " + this.weight + " kg.";
+            }
+        }
     }
-    public string Hunt()
-    {
-      return this.GetType().Name + " hunts.";
-    }
-  }
 }
