6447a7c007f78d31f7e464c7656004de17542c16
janne-nylund <janne.nylund@centria.fi>
2020-05-11 15:46:25 +0300

    Update Hunt() tests

--- test/FaunaTests/FaunaTest.cs
+++ test/FaunaTests/FaunaTest.cs
@@ -147,7 +147,7 @@ public class AnimalTests
     {
         string actual = new Wolf().Hunt();
 
-        Assert.That(actual, Does.Match("Wolf finds food, gains 1kg and weighs now 1 kg.*Wolf does not find food and weighs now 0 kg."));
+        Assert.That(actual, Does.Match("Wolf finds food, gains 1kg and weighs now 1 kg..*Wolf does not find food and weighs now 0 kg."));
 
     }
 
@@ -156,13 +156,13 @@ public class AnimalTests
     {
         string actual = new Bear().Hunt();
 
-        Assert.That(actual, Does.Match("Bear finds food, gains 1kg and weighs now 1 kg.*Bear does not find food and weighs now 0 kg."));
+        Assert.That(actual, Does.Match("Bear finds food, gains 1kg and weighs now 1 kg..*Bear does not find food and weighs now 0 kg."));
     }
 
     [Test]
     public void TigerIsHunting()
     {
         string actual = new Tiger().Hunt();
-        Assert.That(actual, Does.Match("Tiger finds food, gains 1kg and weighs now 1 kg.*Tiger does not find food and weighs now 0 kg."));
+        Assert.That(actual, Does.Match("Tiger finds food, gains 1kg and weighs now 1 kg..*Tiger does not find food and weighs now 0 kg."));
     }
 }
