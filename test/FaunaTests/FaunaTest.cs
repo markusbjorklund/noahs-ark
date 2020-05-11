2122552b5c3f657f88cfc920c372afc06f583d07
janne-nylund <janne.nylund@centria.fi>
2020-05-11 20:23:53 +0300

    Update Hunt() tests

--- test/FaunaTests/FaunaTest.cs
+++ test/FaunaTests/FaunaTest.cs
@@ -146,7 +146,8 @@ public class AnimalTests
     public void WolfIsHunting()
     {
         string actual = new Wolf().Hunt();
-        Assert.That(actual, Does.Match("Wolf.*"));
+        Assert.That(actual, Is.AnyOf(Is.EqualTo("good"), Is.EqualTo("Wolf does not find food and weighs now 0 kg.")));
+        //Assert.That(actual, Does.Match("Wolf.*"));
         //Assert.That(actual, Does.Match("Wolf finds food, gains 1kg and weighs now 1 kg."));
         //Assert.That(actual, Does.Match("Wolf does not find food and weighs now 0 kg."));
 
